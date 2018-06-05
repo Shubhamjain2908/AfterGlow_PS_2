3ccbc60c66acf0f09c345f346a858bba793c87f4
shubham jain <shubhamjain2908@gmail.com>
2018-06-05 21:05:07 +0530

    fetch data api

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/**
+ * Adding data to the database
+ */
 router.post('/addData', function(req, res, next) {
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("ag");
+    
     // dbo.collection("customers").remove({}).then(data => console.log(data)).catch(e => console.log(e)); // deleting all elements
+
     dbo.collection("customers").find({}).toArray(function(err, result) {
       if (err) throw err;
       // console.log(result.length);
+      /**
+       * if records are more than 30 then removing top 5 from it
+       */
       if (result.length > 30) {
         for (let index = 0; index < 5; index++) {
           var myquery = { _id: result[index]._id };
@@ -27,12 +35,19 @@ router.post('/addData', function(req, res, next) {
           });
         }
       }
+
       // save
+      /**
+       * Check for an empty table  
+       */
       if (result.slice(-1)[0] !== undefined) {
         var myobj = { _id: result.slice(-1)[0]._id + 1 , value: req.body.value };   // Last id + 1  
       } else {
         var myobj = { _id: 1 , value: req.body.value };   // Last id + 1
       }
+      /**
+       * Saving the current data 
+       */
       dbo.collection("customers").insertOne(myobj, function(err, res) {
         if (err) throw err;
         console.log("Record inserted");
@@ -44,7 +59,22 @@ router.post('/addData', function(req, res, next) {
     });
   });
 
-  
+});
 
-})
+/**
+ * Fetching data from database
+ */
+router.get('/fetchData', function (req, res, next) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("ag");
+    dbo.collection("customers").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.json({
+        "Result": result.slice(Math.max(result.length - 5, 1))
+      });
+    });
+  });
+});  
+
 module.exports = router;
